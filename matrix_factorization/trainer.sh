3d645de7b2afa61166732775582ea3e796580056
Savannah <128313032+cziqin@users.noreply.github.com>
2024-05-13 10:16:09 -0400

    Update trainer.sh

--- matrix_factorization/trainer.sh
+++ matrix_factorization/trainer.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-for ((a=0; a <= 1; a++)) #a 为--stratified，确定是否采用分层采样。a =0 或者 a= 1
+for ((a=0; a <= 1; a++)) 
 do
-  for ((b=1; b<= 8; b++)) # b 为train_set=test_num：1-7
-  do
+  for ((b=1; b<= 8; b++))
     python mf.py -t $b -s $a
   done
 done
-
-# 就是循环了14次，同构的时候a=1，从1-7迭代一遍，异构的时候a=0,从1-7迭代了一次。
